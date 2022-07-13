3eaeff49cf193c8cdd6b9789e48a58dcf7b586f0
sddsergio <destefanosergio@hotmail.com>
2022-07-12 22:33:47 -0300

    dont env

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
 
 app.get('/', (req, res) => {
     console.log("Petición recibida")
-    res.send('Hola mundo')
+
+    res.status(200).send('Hola mundo')
 })
-    
+
+const PORT = process.env.PORT || 4000
+
 app.listen(4000, () => {
-    console.log('Servidor escuando en el puerto 4000')
+    console.log(`Servidor escuando en el puerto ${PORT}`)
 })
